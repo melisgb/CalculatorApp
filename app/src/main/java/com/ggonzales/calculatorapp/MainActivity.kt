3f5463b2e7c233bc1f595103dc9d9cddfa7f5fbf
melisgb <gloriamelissa1@hotmail.com>
2020-04-20 15:14:20 +0100

    Numbers Button implemented.
    Also with the "." operator working as expected: only one dot in resultsTextBox

--- app/src/main/java/com/ggonzales/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/ggonzales/calculatorapp/MainActivity.kt
@@ -2,11 +2,26 @@ package com.ggonzales.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun numberButtonClick(view: View) {
+        val buttonSelected = view as Button
+        if(resultsEditText.text.toString() == "0"){
+            resultsEditText.text.clear()
+        }
+        if(buttonSelected.text == "."){
+            if(resultsEditText.text.contains(".")) return
+        }
+        resultsEditText.text.append(buttonSelected.text.toString())
     }
 }

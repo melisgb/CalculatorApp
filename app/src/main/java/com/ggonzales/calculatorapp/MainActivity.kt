b5edda38fc75fb3b8baab1e0a5fc57565aa6ac8e
melisgb <gloriamelissa1@hotmail.com>
2020-04-20 23:38:08 +0100

    Calculator working as expected
    Design finished and all buttons are implemented.
    Minor pending fixes:
    - TextView with fixed position
    - Operator % define what to do when adding or multiplying.
    - Round of numbers define.

--- app/src/main/java/com/ggonzales/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/ggonzales/calculatorapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ggonzales.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,17 +14,105 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun numberButtonClick(view: View) {
+        var screenText = screenEditText.text.toString()
         val buttonSelected = view as Button
-        if(resultsEditText.text.toString() == "0"){
-            resultsEditText.text.clear()
+
+        if(isOperatorClicked == true){
+            oldNumber = screenText
+            screenText = ""
+            isOperatorClicked = false
         }
-        if(buttonSelected.text == "."){
-            if(resultsEditText.text.contains(".")) return
+
+        if(screenText == "0"){
+            screenText = ""
         }
-        resultsEditText.text.append(buttonSelected.text.toString())
+        when(buttonSelected.id){
+            R.id.ceroButton ->  screenText += "0"
+            R.id.oneButton ->   screenText += "1"
+            R.id.twoButton ->   screenText += "2"
+            R.id.threeButton -> screenText += "3"
+            R.id.fourButton ->  screenText += "4"
+            R.id.fiveButton ->  screenText += "5"
+            R.id.sixButton ->   screenText += "6"
+            R.id.sevenButton -> screenText += "7"
+            R.id.eightButton -> screenText += "8"
+            R.id.nineButton ->  screenText += "9"
+            R.id.dotButton -> {
+                if(screenText.contains(".")) return
+                if(screenText == "") screenText += "0."
+                else screenText += "."
+            }
+            R.id.signChangeButton ->  {
+                if(screenText.substring(0,1)=="-") {
+                    screenText = screenText.substring(1)
+
+                }
+                else screenText = "-" + screenText
+            }
+            R.id.percentButton ->  {
+                screenText += "%"
+            }
+        }
+        screenEditText.setText(screenText)
+
+    }
+
+    var oper = ""
+    var oldNumber = ""
+    var isOperatorClicked = false
+    fun operatorButtonClick(view: View) {
+        var screenText = screenEditText.text.toString()
+        val buttonSelected = view as Button
+
+        when(buttonSelected.id){
+            R.id.sumButton -> {
+                oper = "+"
+            }
+            R.id.minusButton -> {
+                oper = "-"
+            }
+            R.id.multiplyButton -> {
+                oper = "*"
+            }
+            R.id.divideButton -> {
+                oper = "/"
+            }
+        }
+        oldNumber = screenText
+        isOperatorClicked = true
+    }
 
 
+    fun calculateResultClick(view : View){
+        //  Behaviour Equal Button
+        var newNumber = screenEditText.text.toString()
+        var result = 0.0
 
+        if(newNumber.contains("%") && newNumber[newNumber.length-1] != '%'){
+            val percIdx = newNumber.indexOfFirst{ it == '%'}
+            val numA = newNumber.substring(0,percIdx).toDouble()
+            val numB = newNumber.substring(percIdx+1).toDouble()
+            result = numA * (numB / 100)
+        }
+
+        else {
+            if(newNumber[newNumber.length-1]=='%') {
+                newNumber = (newNumber.substring(0, newNumber.length-1).toDouble() / 100).toString()
+            }
+            when (oper) {
+                "+" -> result = oldNumber.toDouble() + newNumber.toDouble()
+                "-" -> result = oldNumber.toDouble() - newNumber.toDouble()
+                "*" -> result = oldNumber.toDouble() * newNumber.toDouble()
+                "/" -> result = oldNumber.toDouble() / newNumber.toDouble()
+            }
+            result = String.format("%.2f", result).toDouble()
+        }
+            screenEditText.setText(result.toString())
+    }
+
+    fun resetText(view : View){
+        var acButton = view as Button
+        screenEditText.setText("")
 
     }
 }
